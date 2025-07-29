eb87c56d95712c2d89f2998ccda2bee55837ddf0
FeinPeng <1779511225@qq.com>
2025-07-29 17:52:35 +0800

    Initial Neovim config

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+return
+{
+	"nvim-treesitter/nvim-treesitter",
+	branch = 'master',
+	lazy = false,
+	build = ":TSUpdate",
+    config = function()
+        -- treesitter
+        local config = require("nvim-treesitter.configs")
+        config.setup({
+          ensure_installed = {"lua", "javascript"},
+          highlight = { enable = true },
+          indent = { enable = true }
+        })
+    end
+}
