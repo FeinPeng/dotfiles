9cc23332969f03973daa7d31c0cda12595e15bf9
FengPeng <1779511225@qq.com>
2025-08-07 14:11:52 +0800

    nop

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -2,19 +2,18 @@ return
 {
     {
         "mason-org/mason.nvim",
+        lazy = false,
         config = function()
             require("mason").setup()
         end
     },
     {
         "mason-org/mason-lspconfig.nvim",
-        config = function() 
+        config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = {
+                    "tsserver",
                     "lua_ls",
-                    "rust_analyzer"
-                }
-        })
+            })
         end
     },
     {
@@ -23,6 +22,10 @@ return
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
             lspconfig.rust_analyzer.setup({})
+
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
         end
     },
 }
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -8,9 +8,9 @@ return
         -- treesitter
         local config = require("nvim-treesitter.configs")
         config.setup({
-          ensure_installed = {"lua", "javascript"},
-          highlight = { enable = true },
-          indent = { enable = true }
+            auto_install = true,
+              highlight = { enable = true },
+              indent = { enable = true },
         })
     end
 }
